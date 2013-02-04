c3d9fe9e7e369d9816f6ddb5af5a295f0cea8e1f
RJ Bergeron <rbergero@gmail.com>
2013-02-04 00:45:10 -0500

    Initial ESXi 4.x support

--- noarch/account-wrangler.sh
+++ noarch/account-wrangler.sh
@@ -1,7 +1,87 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ $(whoami -u) != "root" ]; then
+if [ $(whoami) != "root" ]; then
   echo "this script needs to be run as root to modify accounts"
+fi
+
+# declare overrides if needed
+if [ $(uname -s) == "VMkernel" ]; then
+  vmkmaj=$(uname -r|sed s'/\..*//')
+  if [ $vmkmaj -lt "5" ]; then
+    getent() {
+      database=${1}
+      case ${database} in
+        group|passwd|shadow)
+          entity=${2}
+          grep ^"${entity}": /etc/${database}
+          ;;
+        *)
+          echo "I didn't expect ${database}!"
+          ;;
+      esac
+    }
+    # overloaded because the builtin doesn't do passwords
+    useradd() {
+      _user=
+      _uid=
+      _pass=
+      _group=
+      _shell=/bin/ash
+      OPTIND=1
+      while getopts "u:g:p:s:" _opt; do
+        case ${_opt} in
+          u)
+            _uid=${OPTARG}
+            ;;
+          p)
+            _pass=${OPTARG}
+            ;;
+          g)
+            _group=${OPTARG}
+            ;;
+        esac
+      done
+      shift $(($OPTIND -1))
+      _user=${1}
+      /sbin/useradd -u ${_uid} -g ${_group} -s ${_shell} ${_user}
+      usermod -g root -p ${_pass} ${_user}
+    }
+    # usermod on ESXi does...what?
+    usermod() {
+      _user=
+      _group=
+      _pass=
+      _gid=
+      OPTIND=1
+      while getopts "g:p:" _opt; do
+        case ${_opt} in
+          g)
+            _group=${OPTARG}
+            ;;
+          p)
+            _pass=${OPTARG}
+            ;;
+        esac
+      done
+      shift $(($OPTIND -1))
+      _user=${1}
+      pwent=$(getent passwd ${_user})
+      if [ -n "${_group}" ]; then
+        _gid=$(getent group ${_group}|awk 'BEGIN{FS=":"} { print $3 }') 
+        npwent=$(echo "${pwent}"|awk 'BEGIN{FS=":";OFS=":"} { print $1,$2,$3,'${_gid}',$5,$6,$7 }')
+        sed -e s@"${pwent}"@"${npwent}"@ /etc/passwd > /etc/passwd.new
+        mv -f /etc/passwd.new /etc/passwd
+      fi
+      if [ -n "${_pass}" ]; then
+        shent=$(getent shadow ${_user})
+        nshent=$(echo -n "${shent}"|awk 'BEGIN{FS=":",OFS=":"} { print $1,"'${_pass}'",$3,$4,5,$6,$7,$8 }')
+        sed -e s@"${shent}"@"${nshent}"@ /etc/shadow > /etc/shadow.new
+        chmod u+w /etc/shadow
+        mv -f /etc/shadow.new /etc/shadow
+        chmod u-w /etc/shadow
+      fi
+    }
+  fi
 fi
 
 # first, add the groups

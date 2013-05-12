4e9c6f13feefb4505fbd9b359311874e5f1baded
RJ Bergeron <rbergero@gmail.com>
2013-05-12 00:33:17 -0400

    Added update-dns script, config template

---
+++ noarch/update-dns.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# load config (DELEGATION, NS, KEYFILE, DOMAIN)
+. /usr/local/etc/update-dns.conf
+
+# call with (add|delete) IP [NAME]
+action=$1
+ip=$2
+name=$(echo $3 | awk -F '.' '{ print $1 }')
+
+loctet=$(echo $ip | awk -F '.' '{ print $4 }')
+
+# attempt to look up name if expiring a deletion
+if [ -z "${name}" ]; then
+	name=$(dig -t PTR ${loctet}.${DELEGATION} @${NS} | grep 'IN PTR' | awk '{ print $5 }' | awk -F '.' '{ print $1 }')
+fi
+
+# err, if we still don't have a name, make something up
+if [ -z "${name}" ]; then
+	name="dynclient-${loctet}"
+fi
+
+# see if we are reissuing an IP
+oldname=$(dig -t PTR ${loctet}.${DELEGATION} @${NS} | grep 'IN PTR' | awk '{ print $5 }' | awk -F '.' '{ print $1 }')
+
+case "$action" in
+	add)
+		UPDATE=$(mktemp)
+		printf "server %s" ${NS} > ${UPDATE}
+		printf "zone %s" ${DOMAIN} >> ${UPDATE}
+		if [ -z "${oldname}" ]; then
+			printf "update delete %s 3600 A" ${oldname}.${DOMAIN} >> ${UPDATE}
+		fi
+		printf "update delete %s 3600 A" ${name}.${DOMAIN} >> ${UPDATE}
+		printf "update add %s 3600 A %s" ${name}.${DOMAIN} ${ip} >> ${UPDATE}
+		printf "send" >> ${UPDATE}
+		nsupdate -k "${KEYFILE}" "${UPDATE}"
+		rm "$UPDATE"
+
+		UPDATE=$(mktemp)
+		printf "server %s" ${NS} > ${UPDATE}
+		printf "zone %s" ${DELEGATION} >> ${UPDATE}
+		printf "update delete %s 3600 PTR" ${loctet}.${DELEGATION} >> ${UPDATE}
+		printf "update add %s 3600 PTR %s" ${loctet}.${DELEGATION} ${name}.${DOMAIN} >> ${UPDATE}
+		printf "send" >> ${UPDATE}
+		nsupdate -k "${KEYFILE}" "${UPDATE}"
+		rm "${UPDATE}"
+		;;
+	delete)
+		if [ -z "${name}" ]; then
+			UPDATE=$(mktemp)
+			printf "server %s" ${NS} > ${UPDATE}
+			printf "zone %s" ${DOMAIN} >> ${UPDATE}
+			printf "update delete %s 3600 A" ${name}.${DOMAIN} >> ${UPDATE}
+			printf "send" >> ${UPDATE}
+			nsupdate -k "${KEYFILE}" "${UPDATE}"
+			rm "$UPDATE"
+		fi
+		UPDATE=$(mktemp)
+		printf "server %s" ${NS} > ${UPDATE}
+		printf "zone %s" ${DELEGATION} >> ${UPDATE}
+		printf "update delete %s 3600 PTR" ${loctet}.${DELEGATION} >> ${UPDATE}
+		printf "send" >> ${UPDATE}
+		nsupdate -k "${KEYFILE}" "${UPDATE}"
+		rm "${UPDATE}"
+		;;
+	*)
+		# noop!
+		;;
+esac

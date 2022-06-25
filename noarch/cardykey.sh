77dd387e683c90b41a8af195959a1b7edc739048
RJ Bergeron <rbergero@gmail.com>
2022-06-25 14:39:45 -0400

    attempt to deal with gpg2 or gpg1 as the gpg2 binary (prefer gpg2)

--- noarch/cardykey.sh
+++ noarch/cardykey.sh
@@ -4,6 +4,10 @@
 pkill scdaemon
 pkill gpg-agent
 
+# if I have a gpg2, prefer that.
+gpgwrap () { gpg "${@}" ; }
+type gpg2 && gpgwrap () { gpg2 "${@}" ; }
+
 # create new masterkey and stubby keys
 
 # display name
@@ -23,9 +27,7 @@ if [ -z "${AUTH_SUBKEY_COUNT}" ] ; then AUTH_SUBKEY_COUNT="2" ; fi
 if [ -z "${SIGNING_SUBKEY_COUNT}" ] ; then SIGNING_SUBKEY_COUNT="1" ; fi
 
 # if I don't have a master key, make one
-gpg2 --list-keys "${GPG_EMAIL}"
-if [ "${?}" -ne 0 ] ; then
-  gpg2 --gen-key --batch << MASTER_PARAMS
+gpgwrap --list-keys "${GPG_EMAIL}" || gpgwrap --gen-key --batch << MASTER_PARAMS
 %no-ask-passphrase
 %no-protection
 Key-Type: rsa
@@ -37,11 +39,10 @@ Expire-Date: ${GPG_EXPIRY}
 Preferences: SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES CAST5 ZLIB BZIP2 ZIP Uncompressed
 ${REVOKER}
 MASTER_PARAMS
-fi
 
 # create unexpired encryption subkeys
-while [ $(gpg2 --list-keys --with-colons "${GPG_EMAIL}" | grep "sub:u:4096" | grep -c "e::::::") != ${ENCRYPTION_SUBKEY_COUNT} ] ; do
-  gpg2 --edit-key --batch --command-fd 0 --passphrase '' "${GPG_EMAIL}" << SUBKEY_PARAMS
+while [ $(gpgwrap --list-keys --with-colons "${GPG_EMAIL}" | grep "sub:u:4096" | grep -c "e::::::") != ${ENCRYPTION_SUBKEY_COUNT} ] ; do
+  gpgwrap --edit-key --batch --command-fd 0 --passphrase '' "${GPG_EMAIL}" << SUBKEY_PARAMS
 %no-ask-passphrase
 %no-protection
 addkey
@@ -53,8 +54,8 @@ SUBKEY_PARAMS
 done
 
 # ditto authentication
-while [ $(gpg2 --list-keys --with-colons "${GPG_EMAIL}" | grep "sub:u:4096" | grep -c "a::::::") != ${AUTH_SUBKEY_COUNT} ] ; do
-  gpg2 --expert --edit-key --batch --command-fd 0 --passphrase '' "${GPG_EMAIL}" << SUBKEY_PARAMS
+while [ $(gpgwrap --list-keys --with-colons "${GPG_EMAIL}" | grep "sub:u:4096" | grep -c "a::::::") != ${AUTH_SUBKEY_COUNT} ] ; do
+  gpgwrap --expert --edit-key --batch --command-fd 0 --passphrase '' "${GPG_EMAIL}" << SUBKEY_PARAMS
 %no-ask-passphrase
 %no-protection
 addkey
@@ -67,8 +68,8 @@ SUBKEY_PARAMS
 done
 
 # and signing
-while [ $(gpg2 --list-keys --with-colons "${GPG_EMAIL}" | grep "sub:u:4096" | grep -c "s::::::") != ${SIGNING_SUBKEY_COUNT} ] ; do
-  gpg2 --edit-key --batch --command-fd 0 --passphrase '' "${GPG_EMAIL}" << SUBKEY_PARAMS
+while [ $(gpgwrap --list-keys --with-colons "${GPG_EMAIL}" | grep "sub:u:4096" | grep -c "s::::::") != ${SIGNING_SUBKEY_COUNT} ] ; do
+  gpgwrap --edit-key --batch --command-fd 0 --passphrase '' "${GPG_EMAIL}" << SUBKEY_PARAMS
 %no-ask-passphrase
 %no-protection
 addkey
@@ -91,8 +92,8 @@ one=""
 two=""
 
 for l in e s a ; do
-_one="${_one} $(gpg2 --list-keys --with-colons "${GPG_EMAIL}" | grep "sub:u:4096" | grep "${l}::::::" |awk -F: '{ key[$7]=$5 } END { asort(key) ; print key[1] }')"
-_two="${_two} $(gpg2 --list-keys --with-colons "${GPG_EMAIL}" | grep "sub:u:4096" | grep "${l}::::::" |awk -F: '{ key[$7]=$5 } END { asort(key) ; print key[2] }')"
+_one="${_one} $(gpgwrap --list-keys --with-colons "${GPG_EMAIL}" | grep "sub:u:4096" | grep "${l}::::::" |awk -F: '{ key[$7]=$5 } END { asort(key) ; print key[1] }')"
+_two="${_two} $(gpgwrap --list-keys --with-colons "${GPG_EMAIL}" | grep "sub:u:4096" | grep "${l}::::::" |awk -F: '{ key[$7]=$5 } END { asort(key) ; print key[2] }')"
 done
 
 for k in ${_one} ; do
@@ -105,14 +106,14 @@ done
 loaded="${one} ${two}"
 
 # export the private keys to files
-gpg2 --export-secret-subkeys -a ${one} > ${GPG_EMAIL}-redone.asc
-gpg2 --export-secret-subkeys -a ${two} > ${GPG_EMAIL}-redtwo.asc
+gpgwrap --export-secret-subkeys -a ${one} > ${GPG_EMAIL}-redone.asc
+gpgwrap --export-secret-subkeys -a ${two} > ${GPG_EMAIL}-redtwo.asc
 
 rm -rf scratch
 mkdir scratch
 echo 'reader-port "Yubico Yubikey NEO OTP+U2F+CCID 01 00"' > scratch/scdaemon.conf
-env GNUPGHOME=$(pwd)/scratch gpg2 --import "${GPG_EMAIL}-redone.asc"
-env GNUPGHOME=$(pwd)/scratch gpg2 --edit-key --batch --command-fd 0 --passphrase '' "${GPG_EMAIL}" << TRUST
+GNUPGHOME=$(pwd)/scratch gpgwrap --import "${GPG_EMAIL}-redone.asc"
+GNUPGHOME=$(pwd)/scratch gpgwrap --edit-key --batch --command-fd 0 --passphrase '' "${GPG_EMAIL}" << TRUST
 trust
 5
 y
@@ -120,16 +121,16 @@ save
 TRUST
 
 # we need to actually get the order the subkeys were written in for card writing
-ekey=$(env GNUPGHOME=$(pwd)/scratch gpg2 --list-keys --with-colons 2>/dev/null | grep 'sub:u:' | grep -n ':e::::::')
+ekey=$(GNUPGHOME=$(pwd)/scratch gpgwrap --list-keys --with-colons 2>/dev/null | grep 'sub:u:' | grep -n ':e::::::')
 ekey=${ekey:0:1}
-akey=$(env GNUPGHOME=$(pwd)/scratch gpg2 --list-keys --with-colons 2>/dev/null | grep 'sub:u:' | grep -n ':a::::::')
+akey=$(GNUPGHOME=$(pwd)/scratch gpgwrap --list-keys --with-colons 2>/dev/null | grep 'sub:u:' | grep -n ':a::::::')
 akey=${akey:0:1}
-skey=$(env GNUPGHOME=$(pwd)/scratch gpg2 --list-keys --with-colons 2>/dev/null | grep 'sub:u:' | grep -n ':s::::::')
+skey=$(GNUPGHOME=$(pwd)/scratch gpgwrap --list-keys --with-colons 2>/dev/null | grep 'sub:u:' | grep -n ':s::::::')
 skey=${skey:0:1}
 
 # now that we know which key is which, push to card. you will be prompted for the admin pin.
 printf '\nrun:\ntoggle\nkey %s\nkeytocard\n1\nsave\n\n' ${skey}
-env GNUPGHOME=$(pwd)/scratch gpg2 --edit-key "${GPG_EMAIL}"
+GNUPGHOME=$(pwd)/scratch gpgwrap --edit-key "${GPG_EMAIL}"
 #toggle
 #key ${skey}
 #keytocard
@@ -138,13 +139,13 @@ env GNUPGHOME=$(pwd)/scratch gpg2 --edit-key "${GPG_EMAIL}"
 #S2CARD
 
 printf '\nrun:\ntoggle\nkey %s\nkeytocard\n2\nsave\n\n' ${ekey}
-env GNUPGHOME=$(pwd)/scratch gpg2 --edit-key "${GPG_EMAIL}"
+GNUPGHOME=$(pwd)/scratch gpgwrap --edit-key "${GPG_EMAIL}"
 
 printf '\nrun:\ntoggle\nkey %s\nkeytocard\n3\nsave\n\n' ${akey}
-env GNUPGHOME=$(pwd)/scratch gpg2 --edit-key "${GPG_EMAIL}"
+GNUPGHOME=$(pwd)/scratch gpgwrap --edit-key "${GPG_EMAIL}"
 
 # export the resulting stubby key
-env GNUPGHOME=$(pwd)/scratch gpg2 --export-secret-subkeys "${GPG_EMAIL}" > ${GPG_EMAIL}-blackone.gpg
+GNUPGHOME=$(pwd)/scratch gpgwrap --export-secret-subkeys "${GPG_EMAIL}" > ${GPG_EMAIL}-blackone.gpg
 
 ekey=""
 akey=""
@@ -154,27 +155,27 @@ pkill gpg-agent
 rm -rf scratch
 mkdir scratch
 echo 'reader-port "Yubico Yubikey NEO OTP+U2F+CCID 01 00"' > scratch/scdaemon.conf
-env GNUPGHOME=$(pwd)/scratch gpg2 --import "${GPG_EMAIL}-redtwo.asc"
-env GNUPGHOME=$(pwd)/scratch gpg2 --edit-key --batch --command-fd 0 --passphrase '' "${GPG_EMAIL}" << TRUST
+GNUPGHOME=$(pwd)/scratch gpgwrap --import "${GPG_EMAIL}-redtwo.asc"
+GNUPGHOME=$(pwd)/scratch gpgwrap --edit-key --batch --command-fd 0 --passphrase '' "${GPG_EMAIL}" << TRUST
 trust
 5
 y
 save
 TRUST
 
-ekey=$(env GNUPGHOME=$(pwd)/scratch gpg2 --list-keys --with-colons 2>/dev/null | grep 'sub:u:' | grep -n ':e::::::')
+ekey=$(GNUPGHOME=$(pwd)/scratch gpgwrap --list-keys --with-colons 2>/dev/null | grep 'sub:u:' | grep -n ':e::::::')
 ekey=${ekey:0:1}
-akey=$(env GNUPGHOME=$(pwd)/scratch gpg2 --list-keys --with-colons 2>/dev/null | grep 'sub:u:' | grep -n ':a::::::')
+akey=$(GNUPGHOME=$(pwd)/scratch gpgwrap --list-keys --with-colons 2>/dev/null | grep 'sub:u:' | grep -n ':a::::::')
 akey=${akey:0:1}
 
 printf '\nrun:\ntoggle\nkey %s\nkeytocard\n2\nsave\n\n' ${ekey}
-env GNUPGHOME=$(pwd)/scratch gpg2 --edit-key "${GPG_EMAIL}"
+GNUPGHOME=$(pwd)/scratch gpgwrap --edit-key "${GPG_EMAIL}"
 
 printf '\nrun:\ntoggle\nkey %s\nkeytocard\n3\nsave\n\n' ${akey}
-env GNUPGHOME=$(pwd)/scratch gpg2 --edit-key "${GPG_EMAIL}"
+GNUPGHOME=$(pwd)/scratch gpgwrap --edit-key "${GPG_EMAIL}"
 
 # export the resulting stubby key
-env GNUPGHOME=$(pwd)/scratch gpg2 --export-secret-subkeys "${GPG_EMAIL}" > ${GPG_EMAIL}-blacktwo.gpg
+GNUPGHOME=$(pwd)/scratch gpgwrap --export-secret-subkeys "${GPG_EMAIL}" > ${GPG_EMAIL}-blacktwo.gpg
 
 pkill scdaemon
 pkill gpg-agent
@@ -186,25 +187,25 @@ shred ${GPG_EMAIL}-redtwo.asc
 
 # import the second key and grab everything in it
 mkdir scratch
-env GNUPGHOME=$(pwd)/scratch gpg2 --import "${GPG_EMAIL}-blacktwo.gpg"
-env GNUPGHOME=$(pwd)/scratch gpg2 --edit-key --batch --command-fd 0 --passphrase '' "${GPG_EMAIL}" << TRUST
+GNUPGHOME=$(pwd)/scratch gpgwrap --import "${GPG_EMAIL}-blacktwo.gpg"
+GNUPGHOME=$(pwd)/scratch gpgwrap --edit-key --batch --command-fd 0 --passphrase '' "${GPG_EMAIL}" << TRUST
 trust
 5
 y
 save
 TRUST
 
-pubkeys="0x$(env GNUPGHOME=$(pwd)/scratch gpg2 --list-keys --with-colons 2>/dev/null | grep 'sub:u:' | grep ':e::::::' | cut -d: -f5)!"
+pubkeys="0x$(GNUPGHOME=$(pwd)/scratch gpgwrap --list-keys --with-colons 2>/dev/null | grep 'sub:u:' | grep ':e::::::' | cut -d: -f5)!"
 
 # now import the first key
-env GNUPGHOME=$(pwd)/scratch gpg2 --import "${GPG_EMAIL}-blackone.gpg"
+GNUPGHOME=$(pwd)/scratch gpgwrap --import "${GPG_EMAIL}-blackone.gpg"
 
 # and grab all the other keys
-for l in $(env GNUPGHOME=$(pwd)/scratch gpg2 --list-keys --with-colons|grep 'sub:u:'|cut -d: -f5,12 | grep -E '(a|s)$') ; do
+for l in $(GNUPGHOME=$(pwd)/scratch gpgwrap --list-keys --with-colons|grep 'sub:u:'|cut -d: -f5,12 | grep -E '(a|s)$') ; do
   pubkeys="${pubkeys} 0x${l%:*}!"
 done
 
-env GNUPGHOME=$(pwd)/scratch gpg2 --export -a ${pubkeys} > ${GPG_EMAIL}-upload.asc
+GNUPGHOME=$(pwd)/scratch gpgwrap --export -a ${pubkeys} > ${GPG_EMAIL}-upload.asc
 
 # now assemble a legacy gpg keyring...
 mkdir one
